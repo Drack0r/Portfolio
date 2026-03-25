8038c9d16c1186c218259a138c947850a14d9750
Antonin <antochevolleau@gmail.com>
2026-03-25 16:00:31 +0100

    feat(projects): add dynamic project list rendering
    - Create structured projectItems data with metadata and badges
    - Render ProjectCard components using map for cleaner code
    - Enable easier updates and additions of new projects
    - Update container width and spacing for improved layout

--- src/components/ui/Badge.jsx
+++ src/components/ui/Badge.jsx
@@ -3,7 +3,13 @@ import Image from "next/image";
 function Badge({ src, alt }) {
   return (
     <div>
-      <Image src={src} alt={alt} width={32} height={32} className="h-8 w-8" />
+      <Image
+        src={src}
+        alt={alt}
+        width={32}
+        height={32}
+        className="shadow-2 h-8 w-8 rounded-full bg-white p-1"
+      />
     </div>
   );
 }
--- src/components/ui/ProjectCard/ProjectOverlay.jsx
+++ src/components/ui/ProjectCard/ProjectOverlay.jsx
@@ -4,7 +4,7 @@ function ProjectOverlay({ isVisible, description }) {
   return (
     <div
       className={clsx(
-        "absolute inset-0 flex items-center justify-center bg-gradient-to-b from-black/50 to-black/60 transition-opacity duration-300",
+        "absolute inset-0 flex items-center justify-center bg-black/60 transition-opacity duration-300",
         isVisible ? "opacity-100" : "opacity-0",
       )}
     >
