6efc6f83d8971847ec6ead03cc86fb2d9278e6de
Antonin <antochevolleau@gmail.com>
2026-04-03 14:28:56 +0200

    refactor(skills-section): map skills from data file
    - Replace hardcoded SkillProgress components with dynamic mapping
    - Import skills data from external JSON file
    - Improve maintainability and scalability of skills list
    - Use key prop based on techStack for list rendering

--- src/components/sections/SkillsSection/SkillsSection.jsx
+++ src/components/sections/SkillsSection/SkillsSection.jsx
@@ -1,4 +1,5 @@
 import { SkillProgress, Title } from "@/components/ui";
+import skillItems from "@/data/skills.json";
 
 function SkillsSection() {
   return (
@@ -7,62 +8,16 @@ function SkillsSection() {
 
       {/* Progress bars container */}
       <div className="mx-auto mb-30 flex w-fit flex-col items-end gap-6">
-        <SkillProgress
-          iconSrc={"/images/icons/html5.png"}
-          iconAlt={"HTML5"}
-          title={"HTML"}
-          color={"orange"}
-          progressLevel={85}
-        />
-        <SkillProgress
-          iconSrc={"/images/icons/css.png"}
-          iconAlt={"CSS"}
-          title={"CSS"}
-          color={"blue"}
-          progressLevel={80}
-        />
-        <SkillProgress
-          iconSrc={"/images/icons/sass.png"}
-          iconAlt={"Sass"}
-          title={"Sass"}
-          color={"pink"}
-          progressLevel={75}
-        />
-        <SkillProgress
-          iconSrc={"/images/icons/tailwindcss.png"}
-          iconAlt={"Tailwind CSS"}
-          title={"Tailwind CSS"}
-          color={"cyan"}
-          progressLevel={60}
-        />
-        <SkillProgress
-          iconSrc={"/images/icons/javascript.png"}
-          iconAlt={"JavaScript"}
-          title={"JavaScript"}
-          color={"yellow"}
-          progressLevel={65}
-        />
-        <SkillProgress
-          iconSrc={"/images/icons/react.png"}
-          iconAlt={"React"}
-          title={"React"}
-          color={"teal"}
-          progressLevel={65}
-        />
-        <SkillProgress
-          iconSrc={"/images/icons/nextjs.png"}
-          iconAlt={"Next.js"}
-          title={"Next.js"}
-          color={"gray"}
-          progressLevel={55}
-        />
-        <SkillProgress
-          iconSrc={"/images/icons/github.png"}
-          iconAlt={"Github"}
-          title={"Git & GitHub"}
-          color={"black"}
-          progressLevel={70}
-        />
+        {skillItems.map((skill) => (
+          <SkillProgress
+            key={skill.techStack}
+            iconSrc={skill.iconSrc}
+            iconAlt={skill.iconAlt}
+            title={skill.techStack}
+            color={skill.color}
+            progressLevel={skill.progressLevel}
+          />
+        ))}
       </div>
     </section>
   );
